7d0ba3211022a93c2ab8ebe571114063b758ae40
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-06-29 13:35:29 -0600

    feat(metadata): add UpdateUserMetadata function to update user metadata in Clerk

--- clerk/metadata.go
+++ clerk/metadata.go
@@ -2,21 +2,17 @@ package clerk
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"os"
 
 	"github.com/clerk/clerk-sdk-go/v2"
 	"github.com/clerk/clerk-sdk-go/v2/user"
-	"github.com/joho/godotenv"
 )
 
 var globalCtx context.Context
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Println("Warning: .env file not found, using system environment variables")
-	}
 
 	globalCtx = context.Background()
 	clerkAPIKey := os.Getenv("CLERK_SECRET_KEY")
@@ -27,23 +23,15 @@ func init() {
 	clerk.SetKey(clerkAPIKey)
 }
 
-func CreateUser(userParams *user.CreateParams) (*clerk.User, error) {
-	newUser, err := user.Create(globalCtx, userParams)
+func UpdateUserMetadata(userId string, metadata map[string]string) error {
+	jsonData, err := json.Marshal(metadata)
+	if err != nil {
+		return err
+	}
 
-	return newUser, err
-}
-
-func GetUser(userId string) (*clerk.User, error) {
-	userDetails, err := user.Get(globalCtx, userId)
-	return userDetails, err
-}
-
-func ListUsers() ([]*clerk.User, error) {
-	users, err := user.List(globalCtx, &user.ListParams{})
-	return users.Users, err
-}
-
-func DeleteUser(userId string) error {
-	_, err := user.Delete(globalCtx, userId)
+	rawMessage := json.RawMessage(jsonData)
+	_, err = user.Update(globalCtx, userId, &user.UpdateParams{
+		PublicMetadata: &rawMessage,
+	})
 	return err
 }
