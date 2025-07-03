0c551fc7ce9aa90ce06319d1536a0c4394664c8b
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-07-03 12:20:42 -0600

    feat(clerk): add organization management functions for user organization memberships and metadata handling

---
+++ clerk/organizations.go
@@ -0,0 +1,55 @@
+package clerk
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/clerk/clerk-sdk-go/v2"
+	"github.com/clerk/clerk-sdk-go/v2/organization"
+	"github.com/clerk/clerk-sdk-go/v2/user"
+)
+
+func init() {
+	clerkAPIKey := os.Getenv("CLERK_SECRET_KEY")
+	clerk.SetKey(clerkAPIKey)
+}
+
+func GetUserOrganizations(userId string) (*clerk.OrganizationMembershipList, error) {
+	orgMemberships, err := user.ListOrganizationMemberships(context.Background(), userId, &user.ListOrganizationMembershipsParams{})
+
+	if err != nil {
+		log.Printf("Error getting organization memberships: %v", err)
+	}
+
+	return orgMemberships, nil
+}
+
+func GetOrganizationMetadata(organizationId string) (map[string]interface{}, error) {
+	organization, err := organization.Get(context.Background(), organizationId)
+	if err != nil {
+		return nil, err
+	}
+
+	var metadata map[string]interface{}
+	if err := json.Unmarshal(organization.PublicMetadata, &metadata); err != nil {
+		return nil, err
+	}
+
+	return metadata, nil
+}
+
+func UpdateOrganizationMetadata(organizationId string, metadata map[string]interface{}) error {
+	jsonData, err := json.Marshal(metadata)
+	if err != nil {
+		return err
+	}
+
+	rawMessage := json.RawMessage(jsonData)
+	_, err = organization.Update(context.Background(), organizationId, &organization.UpdateParams{
+		PublicMetadata: &rawMessage,
+	})
+
+	return err
+}
